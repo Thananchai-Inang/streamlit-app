ad390fac6e995a03d7910adb305c0cb096b733b7
Dawn <inangthananchai@gmail.com>
2023-11-23 21:40:39 +0700

    modify mockup.py

--- streamlit/app/mockup.py
+++ streamlit/app/mockup.py
@@ -25,8 +25,8 @@ mongo_db = MongoDB()
 data_list = list()
 for date in range(1, 100, 1):
     data_list.append({
-        'Name': 'genmockdata',
-        'Date': date,
+        'name': 'genmockdata',
+        'date': date,
         'waterlevel': random.uniform(100, 120),  # Use random.uniform for float values
         'waterdrain': random.uniform(90, 180)  # Use random.uniform for float values
     })
