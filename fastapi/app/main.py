a94414fcf2b8d323b4d1ea53859bc9d00b200a9d
Dawn <inangthananchai@gmail.com>
2023-11-24 04:01:48 +0700

    change main.py to asyncio

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,6 @@
 import uvicorn
-
+import asyncio
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=80, reload=True)
+
+    asyncio.create_task(uvicorn.run("server.app:app", host="0.0.0.0", port=80, reload=True))
+    asyncio.get_event_loop().run_forever()
